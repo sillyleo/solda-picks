1606b1ba9e7ed2ee36f0aa3e7b9e699fd291d97a
Leo <leolin@me.com>
2015-09-02 16:57:42 +0800

    add web notification

--- js/effect.js
+++ js/effect.js
@@ -1,43 +1,9 @@
-
-        // <!-- jquery + 動態背景色 -->
-        // <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        // <script src="js/jquery.adaptive-backgrounds.js"></script>
-        // <script src="js/effect.js"></script>
-
-
-
-        <!-- jQuery -->
-        // <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-        // <script src="js/jquery.lazyload.min.js"></script>
-        // <script src="js/effect.js"></script>
-
-
 $(document).ready(function(){
 
-// for article
-
-// var article      = {
-//   selector:             '.backdrop',
-//   parent:               '.big-bg',
-//   exclude:              [ 'rgb(0,0,0)', 'rgba(255,255,255)' ],
-//   normalizeTextColor:   false,
-//   normalizedTextColors:  {
-//     light:      "#fff",
-//     dark:       "#000"
-//   },
-//   lumaClasses:  {
-//     light:      "ab-light",
-//     dark:       "ab-dark"
-//   }
-// };
-// $.adaptiveBackground.run(article);
-
-// $.adaptiveBackground.run();
-
-
-  $("img").lazyload();
-  $("div.mdl-card").lazyload({
-      effect : "fadeIn"
+  $("div.b-lazy").lazyload({
+      effect : "fadeIn",
+      container: $(".mdl-grid.main-content")
   });
+	$(window).scroll();
 
 });
