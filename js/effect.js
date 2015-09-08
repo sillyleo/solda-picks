baf52f91b0b40bd13e76c66454432919aafb7e01
Leo <leolin@me.com>
2015-09-08 14:01:32 +0800

    update footer

--- js/effect.js
+++ js/effect.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
 
   $("div.mdl-card, .b-lazy").lazyload({
       effect : "fadeIn",
-      container: $(".mdl-grid.main-content")
+      container: $(".mdl-grid")
   });
-	$(window).scroll();
+
+	// $('window').resize();
 
 });
